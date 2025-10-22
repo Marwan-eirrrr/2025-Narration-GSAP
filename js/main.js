44a00fcb41fc314aa5a8456dfe17a67d19639a5e
el idrissi rachidi <elidrissirachidi@MacBook-Pro-de-el.local>
2025-10-22 15:59:47 +0200

    Adding basic files for projetcs

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ let tl = gsap.timeline();
 tl.to("#mouv1", {duration: 1, x: 35})
   .to("#mouv2", {duration: 2, x: 35})
 
+/**Draggable section 6 */ 
+Draggable.create("#logoInsta", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logoInsta' ).style.width = '100px'
+    }
+})
