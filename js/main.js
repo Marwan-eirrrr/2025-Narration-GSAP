587fdf78727c15fce4c9c3b92a737211048348fe
el idrissi rachidi <elidrissirachidi@MacBook-Pro-de-el.local>
2025-10-22 15:06:01 +0200

    Adding basic files for projetcs

---
+++ js/main.js
@@ -0,0 +1,5 @@
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+let tl = gsap.timeline();
+tl.to("#mouv1", {duration: 1, x: 35})
+  .to("#mouv2", {duration: 2, x: 35})
+
