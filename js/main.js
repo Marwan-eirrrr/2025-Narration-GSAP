e36efcf80a51b61f55aca2f4ff87c3d2c55cd1ae
el idrissi rachidi <elidrissirachidi@MacBook-Pro-de-el.local>
2025-10-23 11:59:10 +0200

    Adding basic files for projetcs

--- js/main.js
+++ js/main.js
@@ -12,3 +12,104 @@ Draggable.create("#logoInsta", {
         document.getElementById( 'logoInsta' ).style.width = '100px'
     }
 })
+
+Draggable.create("#logoInsta", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logoInsta' ).style.width = '100px'
+    }
+})
+/** Deuxieme draggable section 6 */
+
+Draggable.create("#logolinkdin", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logolinkdin' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logolinkdin", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logolinkdin' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logolinkdin", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logolinkdin' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logolinkdin", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logolinkdin' ).style.width = '100px'
+    }
+})
+
+
+/** Troisieme draggable section 6 */
+
+Draggable.create("#logotiktok", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logotiktok' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logolinkdin", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logotiktok' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logotiktok", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logotiktok' ).style.width = '100px'
+    }
+})
+
+Draggable.create("#logotiktok", {
+    type: "x, y",
+    bounds: "#section6", cursor: 'grab',
+    
+    onDrag: function(){
+        document.getElementById( 'logotiktok' ).style.width = '100px'
+    }
+})
+/** annimation section5 */
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#photoSection5 img", {
+  scrollTrigger: {
+    trigger: "#section5",      
+    start: "top 80%",          
+    toggleActions: "play none none reverse", 
+  },
+  opacity: 0,                  
+  y: 100,                      
+  duration: 1,                 
+  stagger: 0.2,                
+  ease: "power2.out"
+});
+
