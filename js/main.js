076d3761fc34105c79ce7b52d8266559a343a08f
el idrissi rachidi <elidrissirachidi@MacBook-Pro-de-el.local>
2025-10-23 14:54:22 +0200

    Adding basic files for production

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
-gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);   /*--vous monsieur*/
+
+/** annimation section1 */
+
 let tl = gsap.timeline();
 tl.to("#mouv1", {duration: 1, x: 35})
   .to("#mouv2", {duration: 2, x: 35})
 
+/** annimation section5 */
+
+gsap.from(".section5__photos", {
+    scrollTrigger: {
+        trigger: "#section5",    
+        markers: true,  
+        start: "top 50%", 
+        end:"top 20%",     
+        scrub:1,    
+        toggleActions: "play none reverse reset",
+    },
+
+  opacity: 0,                  
+  y: 100,                      
+  duration: 1,                 
+  stagger: 0.2,                
+  ease: "power2.out"
+});
+
 /**Draggable section 6 */ 
-Draggable.create("#logoInsta", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logoInsta' ).style.width = '100px'
-    }
-})
 
 Draggable.create("#logoInsta", {
     type: "x, y",
     bounds: "#section6", cursor: 'grab',
     
     onDrag: function(){
-        document.getElementById( 'logoInsta' ).style.width = '100px'
+        document.getElementById('logoInsta').style.width = '100px'
     }
 })
 /** Deuxieme draggable section 6 */
@@ -28,88 +42,18 @@ Draggable.create("#logolinkdin", {
     bounds: "#section6", cursor: 'grab',
     
     onDrag: function(){
-        document.getElementById( 'logolinkdin' ).style.width = '100px'
+        document.getElementById('logolinkdin').style.width = '100px'
     }
 })
-
-Draggable.create("#logolinkdin", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logolinkdin' ).style.width = '100px'
-    }
-})
-
-Draggable.create("#logolinkdin", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logolinkdin' ).style.width = '100px'
-    }
-})
-
-Draggable.create("#logolinkdin", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logolinkdin' ).style.width = '100px'
-    }
-})
-
-
 /** Troisieme draggable section 6 */
 
-Draggable.create("#logotiktok", {
+Draggable.create("#logotiktok",{
     type: "x, y",
-    bounds: "#section6", cursor: 'grab',
+    bounds: "#section6",cursor:'grab',
     
     onDrag: function(){
-        document.getElementById( 'logotiktok' ).style.width = '100px'
+        document.getElementById('logotiktok').style.width ='100px'
     }
 })
 
-Draggable.create("#logolinkdin", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logotiktok' ).style.width = '100px'
-    }
-})
-
-Draggable.create("#logotiktok", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logotiktok' ).style.width = '100px'
-    }
-})
-
-Draggable.create("#logotiktok", {
-    type: "x, y",
-    bounds: "#section6", cursor: 'grab',
-    
-    onDrag: function(){
-        document.getElementById( 'logotiktok' ).style.width = '100px'
-    }
-})
-/** annimation section5 */
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.from("#photoSection5 img", {
-  scrollTrigger: {
-    trigger: "#section5",      
-    start: "top 80%",          
-    toggleActions: "play none none reverse", 
-  },
-  opacity: 0,                  
-  y: 100,                      
-  duration: 1,                 
-  stagger: 0.2,                
-  ease: "power2.out"
-});
 
