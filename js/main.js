957dfe282b7839aa8f47111212067204886e66ca
el idrissi rachidi <elidrissirachidi@macbook-pro-de-el.home>
2025-11-02 18:47:11 +0100

    Adding basic files for production

--- js/main.js
+++ js/main.js
@@ -6,12 +6,26 @@ let tl = gsap.timeline();
 tl.to("#mouv1", {duration: 1, x: 35})
   .to("#mouv2", {duration: 2, x: 35})
 
-/** annimation section5 */
+/** annimation section2 */
+ /*explication du carouselle pour vous motre que j'ai compris a l'aide de mes camarade et de la bibliotheque gsap  */
 
+let tlCarousel = gsap.timeline({
+    repeat: 1,
+    yoyo: true
+});
+tlCarousel.to(".carousel__img",{
+    xPercent: -90,
+    duration: 5,
+});
+ /*gsap plus aide de membre de la classe en gros j'ai compris les premiere lignege sont pour placer les phot puis les ligne d'apres pour commencer l'annimation quan on arive sur la page et la repeter une fois et la derniere pour quelle bouge sur l axe x */
+ /*pour que l'annaimation ce decale de la gauche vers la droit*/
+
+
+/** annimation section5 */
 gsap.from(".section5__photos", {
     scrollTrigger: {
         trigger: "#section5",    
-        markers: true,  
+       // markers: true,  
         start: "top 50%", 
         end:"top 20%",     
         scrub:1,    
